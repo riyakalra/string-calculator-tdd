6be84b5ba49735652e0e1affc64ff48940e978a4
riyakalra <riyakalra59@gmail.com>
2024-10-17 21:32:24 +0530

    Test: Failing test case for negative numbers

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -23,3 +23,7 @@ test('returns the sum when numbers are separated by commas and newlines', () => 
 test('returns the sum when numbers are separated by custom delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
+
+test('throws an error for negative numbers', () => {
+    expect(() => add("1,-2,-3")).toThrow("Negative numbers not allowed: -2, -3");
+});
