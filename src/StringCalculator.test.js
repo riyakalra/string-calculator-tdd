03361e7e26d5fe8b77f1bd915bd7fe8022b91d9a
riyakalra <riyakalra59@gmail.com>
2024-10-17 18:19:07 +0530

    Test: Failing test case for single number input

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,7 @@ import { add } from './StringCalculator';
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns the number itself when one number is provided', () => {
+    expect(add("1")).toBe(1);
+  });
