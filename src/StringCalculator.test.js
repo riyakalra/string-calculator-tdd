41de4878caf4bc8ee8b8e8e1156502c920ea57f0
riyakalra <riyakalra59@gmail.com>
2024-10-17 21:12:28 +0530

    Test: Passing test case for multiple numbers input

--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,13 +1,17 @@
 import { add } from './StringCalculator';
 
 test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
 });
 
 test('returns the number itself when one number is provided', () => {
     expect(add("1")).toBe(1);
-  });
+});
 
 test('returns the sum of two numbers separated by a comma', () => {
     expect(add("1,5")).toBe(6);
-});  
+});
+
+test('returns the sum of multiple numbers separated by commas', () => {
+    expect(add("1,2,3,4")).toBe(10);
+});
